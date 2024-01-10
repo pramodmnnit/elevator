e6b9d0b525db2bfd699f5da1ac2a664249f7aa79
Pramod Yadav <pramod.k@embibe.com>
2024-01-10 08:52:38 +0530

    models and exceptions package added

---
+++ src/test/java/com/low/level/design/elevator/ElevatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.low.level.design.elevator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
